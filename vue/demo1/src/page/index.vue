e5f087801e6366eb8a8a192877dd296a60f326fd
robin <574244038@qq.com>
2020-04-14 23:28:42 +0800

    update

--- vue/demo1/src/page/index.vue
+++ vue/demo1/src/page/index.vue
@@ -1,29 +1,37 @@
 <template>
     <div class="home">
         <div>{{ name }}</div>
+        <hello></hello>
     </div>
 </template>
 
 <script>
-    // import findIndex from "lodash.findindex";
-    export default {
-        data() {
-            return {
-                name: 123,
-                arr: [1, 2, 3]
-            };
-        },
-        created() {
-            console.log(123);
-        },
-        methods: {
-            find() {}
-        }
-    };
+/* eslint-disable */
+import hello from "../components/HelloWorld";
+// import findIndex from "lodash.findindex";
+export default {
+    components: {
+        hello
+    },
+    data() {
+        return {
+            name: 123,
+            arr: [1, 2, 3]
+        };
+    },
+    created() {
+        console.log(123);
+    },
+    methods: {
+        find() {}
+    },
+    computed: {},
+    watch: {}
+};
 </script>
 
 <style lang="less">
-    .home {
-        font-size: 12px;
-    }
+.home {
+    font-size: 12px;
+}
 </style>
